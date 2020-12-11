039ea7df52d6b30224b44d97a2629c290ecc67d9
DonnyLVu <donnyvu897@gmail.com>
2020-12-10 18:15:06 -0800

    add working join on get id, app.test.js is where that join happens

--- __Test__/app.test.js
+++ __Test__/app.test.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool.js');
-// const request = require('supertest');
-// const app = require('../lib/app.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+const Menu = require('../lib/models/Menus.js');
+const Restaurant = require('../lib/models/Restaurants.js');
 
 describe('PLACEHOLDER', () => {
   beforeEach(() => {
@@ -11,5 +13,72 @@ describe('PLACEHOLDER', () => {
     return pool.end();
   });
 
+  it('Create a restaurant', async () => {
+    const res = await request(app)
+      .post('/restaurants')
+      .send({
+        location: 'location for create test'
+      });
+    expect(res.body).toEqual({
+      id: '1',
+      location: 'location for create test'
+    });
+  });
 
+  it('Get ALL restaurants', async () => {
+    const restaurants = await Promise.all([
+      {
+        location: 'location 1 for all'
+      },
+      {
+        location: 'location 2 for all'
+      },
+      {
+        location: 'location 3 for all'
+      }
+    ].map(restaurant => Restaurant.insert(restaurant)));
+
+    const res = await request(app)
+      .get('/restaurants');
+    expect(res.body).toEqual(expect.arrayContaining(restaurants));
+    expect(res.body).toHaveLength(restaurants.length);
+  });
+
+  it('Get all menus from reasuratnt id', async () => {
+    await Promise.all([
+      { name: 'menu decoy id 1' },
+      { name: 'menu decoy id 2' },
+      { name: 'menu decoy id 3' }
+    ].map(menu => Menu.insert(menu)));
+    const restaurant = await Restaurant.insert({ location: 'location for get by id', menus: ['menu decoy id 1', 'menu decoy id 2'] });
+    const res = await request(app)
+      .get(`/restaurants/${restaurant.id}`);
+    expect(res.body).toEqual({
+      ...restaurant,
+      menus: ['menu decoy id 1', 'menu decoy id 2']
+    });
+  });
+
+  it('Updates a restauramt', async () => {
+    const restaurant = await Restaurant.insert({ location: 'location for update' });
+    const res = await request(app)
+      .put(`/restaurants/${restaurant.id}`)
+      .send({
+        location: 'return new location for update'
+      });
+    expect(res.body).toEqual({
+      id: '1',
+      location: 'return new location for update'
+    });
+  });
+
+  it('Deletes a restaurant', async () => {
+    const restaurant = await Restaurant.insert({ location: 'location for delete' });
+    const res = await request(app)
+      .delete(`/restaurants/${restaurant.id}`);
+    expect(res.body).toEqual({
+      id: '1',
+      location: 'location for delete'
+    });
+  });
 });
